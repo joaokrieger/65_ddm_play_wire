017f1603f77fe3b7be44dfdffa48a86fb83f533d
joaokrieger <joaoeduardokrieger123@gmail.com>
2023-09-08 21:50:54 -0300

    Inicio do desenvolvimento do ReviewFormFragment

--- PlayWire/app/src/main/java/com/ddm/playwire/MenuActivity.java
+++ PlayWire/app/src/main/java/com/ddm/playwire/MenuActivity.java
@@ -40,7 +40,7 @@ public class MenuActivity extends AppCompatActivity {
         });
     }
 
-    private void replaceFragment(Fragment fragment){
+    public void replaceFragment(Fragment fragment){
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.fragment_layout, fragment);
